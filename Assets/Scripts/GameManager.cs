e42d08edd97bc9e362bdf2f72c8ff341a0c253ce
chopin890612 <chopin890612@gmail.com>
2024-01-28 02:49:22 +0800

    feat: Drag v1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -14,9 +14,12 @@ public class GameManager : MonoBehaviour
     public GameObject containerRoot;
     public static GameManager instance;
 
+    public GameObject draggedItem;
+
     //Private
     StringItemDictionary _itemPrefab = new StringItemDictionary();
     List<string> _playerHadItme = new List<string>();
+    int _createdCount = 0;
 
     // Start is called before the first frame update
     void Start()
@@ -33,19 +36,40 @@ public class GameManager : MonoBehaviour
         }
 
         //Init _playerHadItme, have basic elemental
+        foreach (var item in _itemPrefab)
+        {
+            _playerHadItme.Add(item.Value.Name);
+        }
 
+        CreateAllHadItem();
     }
     
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButton(0)) 
+        {
+            if (draggedItem != null)
+            {
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.z = 0;
+                draggedItem.transform.position = mousePos;
+            }
+        }
+        else
+        {
+            if(draggedItem != null)
+            {
+                draggedItem.GetComponent<Image>().raycastTarget = true;
+                draggedItem = null;
+            }
+        }
     }
 
     public List<string> GetAllPlayerHadItem() { return  _playerHadItme; }
 
-    public void CreateOnField(string itemName) { CreateItem(itemName, fieldRoot.transform); }
-    public void CreateOnContainer(string itemName) { CreateItem(itemName, containerRoot.transform); }
+    public GameObject CreateOnField(string itemName) { return CreateItem(itemName, fieldRoot.transform); }
+    public GameObject CreateOnContainer(string itemName) { return CreateItem(itemName, containerRoot.transform); }
     private GameObject CreateItem(string itemName, Transform parent)
     {
         if(itemName == null) return null;
@@ -57,5 +81,30 @@ public class GameManager : MonoBehaviour
         _itemPrefab.TryGetValue(itemName, out obj.Item);
         obj.gameObject.GetComponent<Image>().sprite = obj.Item.Texture;
         return obj.gameObject;
+    }
+
+    public void CreateFromSelection(string itemName)
+    {
+        var newDrag = CreateOnField(itemName);
+        newDrag.GetComponent<ItemMono>().IsSelected = false;
+        newDrag.GetComponent<Image>().raycastTarget = false;
+        draggedItem = newDrag;
+    }
+
+    public void BeginDrag(GameObject itemObj)
+    {
+        itemObj.GetComponent<Image>().raycastTarget = false;
+        itemObj.transform.SetSiblingIndex(itemObj.transform.parent.childCount - 1);
+        draggedItem = itemObj;
+
+    }
+
+    private void CreateAllHadItem()
+    {
+        for(; _createdCount < _playerHadItme.Count; _createdCount++)
+        {
+            var selection = CreateOnContainer(_playerHadItme[_createdCount]);
+            selection.GetComponent<ItemMono>().IsSelected = true;
+        }
     }
 }
--- Assets/Scripts/ItemMono.cs
+++ Assets/Scripts/ItemMono.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
 public class ItemMono : MonoBehaviour, IPointerDownHandler
 {
     public Item Item;
+    public bool IsSelected = false;
 
     public void OnPointerDown(PointerEventData eventData)
     {
-        GameManager.instance.CreateOnContainer(Item.Name);
+        if(IsSelected)
+        {
+            GameManager.instance.CreateFromSelection(Item.Name);
+        }
+        else
+        {
+            GameManager.instance.BeginDrag(gameObject);
+        }
     }
 
     public void TryCombine(ItemMono item)
