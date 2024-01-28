916793981ff3c4e99fe77be490c72347424c7b9a
chopin890612 <chopin890612@gmail.com>
2024-01-28 14:37:53 +0800

    fix: windows new line problem

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -30,7 +30,7 @@ public class GameManager : MonoBehaviour
     {
         _initialItem = new List<string>();
         TextAsset initList = Resources.Load<TextAsset>("init_list");
-        string[] lines = initList.text.Split('\n');
+        string[] lines = initList.text.Split("\r\n");
         foreach (string line in lines)
         {
             _initialItem.Add(line);
